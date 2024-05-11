bc92755e0e367953e3bce6e2cef2f47c2086b1f5
Kain Nhantumbo <nhantumbok@gmail.com>
2024-05-11 23:45:31 +0200

    fix: updated env port variable name"

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import Bootstrap from "./modules/core/app.module";
 import CreateApp from "./modules/core/app.service";
 
 const app = new CreateApp().getAppInstance();
-const server = new Bootstrap({ app, port: Number(process.env.DB_URI) || 8080 });
+const server = new Bootstrap({ app, port: Number(process.env.PORT) || 8080 });
 
 server.start();
