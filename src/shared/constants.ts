882e588ad3b303324cb0dc949c2cbc04cdb23af2
Kain Nhantumbo <kain.nhantumbo@void.co.mz>
2024-07-19 22:32:21 +0200

    chore: ran linter

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,7 +3,6 @@ export const DATE_FORMAT = "MMM D, YYYY | hh:mm:ss";
 export const CLOUD_USER_IMAGE_REPOSITORY: string = "/toono-community-api/users";
 export const CLOUD_POSTS_IMAGE_REPOSITORY: string = "/toono-community-api/posts";
 
-
 export const POSTS_COLUMN_FIELDS = [
   "title",
   "slug",
